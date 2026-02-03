fb9183ed19772bd6fb7949e346f2b638b4b970cd
Nolan Martzall <Nolan.M@themechanisms.org>
2026-02-02 20:17:47 -0500

    Updated Climber file and added other (Unfinished) IO files

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -3,12 +3,27 @@ package frc.robot.subsystems.climber;
 *   The use of SparkMax motors are is a placeholder.
 *   We can swap this out for the actual motor controller we use for the climber later.
 */
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.MotorType;
 
 public class Climber {
 
-    public void setClimberSpeed(double speed) {
-        motor.set(speed);
+    private final ClimberIO climberIO;
+    private final ClimberIO.ClimberIOInputs inputs = new ClimberIO.ClimberIOInputs();
+
+    public Climber(ClimberIO climberio) {
+        this.climberIO = climberio;
     }
+
+    public void setMotorOpenLoop(double percent) {
+        this.climberIO.setMotorOpenLoop(percent);
+    }
+
+    public void setMotorVelocity(double velocityRps) {
+        this.climberIO.setMotorVelocity(velocityRps);
+    }
+
+    public void stopMotor() {
+        this.climberIO.stopMotor();
+    }
+
+
 }
