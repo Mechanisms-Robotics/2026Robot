e5bed4807e3398064141189e0a4db15a79c80fc6
Nolan Martzall <Nolan.M@themechanisms.org>
2026-02-14 11:12:44 -0500

    Tried some new logging code. (I think?)

--- src/main/java/frc/robot/subsystems/LED/LED.java
+++ src/main/java/frc/robot/subsystems/LED/LED.java
@@ -1,11 +1,12 @@
 package frc.robot.subsystems.LED;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.littletonrobotics.junction.Logger;
 
 public class LED {
 
     private final LEDIO ledIO;
     private final LEDIO.LEDIOInputs inputs = new LEDIO.LEDIOInputs();
+    
 
     public LED(LEDIO ledio) {
         this.ledIO = ledio;
@@ -22,7 +23,7 @@ public class LED {
 
     public void update() {
         this.ledIO.updateInputs(this.inputs);
-        SmartDashboard.putBoolean("LED Connected", this.inputs.LEDConnected);
-        SmartDashboard.putString("Last LED Message", this.inputs.lastLEDMessage);
+        Logger.recordOutput("LED Connected", this.inputs.LEDConnected);
+        Logger.recordOutput("Last LED Message", this.inputs.lastLEDMessage);
     }
 }
