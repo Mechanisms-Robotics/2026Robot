1bb2ae24aa3bb465cc3e37d69394a4e4f20cee50
Nolan Martzall <Nolan.M@themechanisms.org>
2026-02-14 11:44:37 -0500

    Finally figured out the logging for LED! :tada:

--- src/main/java/frc/robot/subsystems/LED/LED.java
+++ src/main/java/frc/robot/subsystems/LED/LED.java
@@ -2,7 +2,9 @@ package frc.robot.subsystems.LED;
 
 import org.littletonrobotics.junction.Logger;
 
-public class LED {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LED extends SubsystemBase{
 
     private final LEDIO ledIO;
     private final LEDIO.LEDIOInputs inputs = new LEDIO.LEDIOInputs();
@@ -21,9 +23,16 @@ public class LED {
         this.ledIO.sendMessage1();
     }
 
-    public void update() {
+    @Override
+    public void periodic() {
         this.ledIO.updateInputs(this.inputs);
         Logger.recordOutput("LED Connected", this.inputs.LEDConnected);
         Logger.recordOutput("Last LED Message", this.inputs.lastLEDMessage);
     }
+    
+    // public void update() {
+    //     this.ledIO.updateInputs(this.inputs);
+    //     Logger.recordOutput("LED Connected", this.inputs.LEDConnected);
+    //     Logger.recordOutput("Last LED Message", this.inputs.lastLEDMessage);
+    // }
 }
