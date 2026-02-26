0ee6fa4a9bab0a49a57e36a8eab6bc3997fa9dbd
Micah Maphet <micahmaphet@gmail.com>
2026-02-26 13:47:50 -0500

    added flywheel subsystem

---
+++ src/main/java/frc/robot/subsystems/shooter/flywheel/Flywheel.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems.shooter.flywheel;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Flywheel extends SubsystemBase {
+    private final FlywheelIO io;
+    private final FlywheelIOInputsAutoLogged inputs = new FlywheelIOInputsAutoLogged();
+    public Flywheel(FlywheelIO io) {
+        this.io = io;
+    }
+
+    @Override
+    public void periodic() {
+        this.io.updateInputs(this.inputs);
+        Logger.processInputs("Turret", this.inputs);
+    }
+
+    public void setVelocity(double rps) {
+        io.setVelocity(rps);
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelIO.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.shooter.flywheel;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface FlywheelIO {
+    @AutoLog
+    public static class FlywheelIOInputs {
+        public double rps; // rotations per second
+    }
+    
+    default void updateInputs(FlywheelIOInputs inputs) {}
+
+    default void setVelocity(double rps) {}
+}
