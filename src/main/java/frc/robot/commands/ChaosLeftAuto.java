262a87ddc436a874c4c63ef5f1fc1723ce41c3fe
Micah Maphet <micahmaphet@gmail.com>
2026-03-04 16:14:16 -0500

    added shooting to ChaosLeftAuto.java

--- src/main/java/frc/robot/commands/ChaosLeftAuto.java
+++ src/main/java/frc/robot/commands/ChaosLeftAuto.java
@@ -7,38 +7,31 @@ import choreo.trajectory.SwerveSample;
 import choreo.trajectory.Trajectory;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.shooter.flywheel.Flywheel;
-import frc.robot.subsystems.shooter.flywheel.FlywheelIO;
+import frc.robot.subsystems.shooter.hood.Hood;
 import frc.robot.subsystems.feeder.Feeder;
-import frc.robot.subsystems.feeder.FeederIO;
-import frc.robot.CONSTANTS;
 import frc.robot.commands.ShootCommands.ManualShoot;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class ChaosLeftAuto extends SequentialCommandGroup {
-    public ChaosLeftAuto(Drivetrain drivetrain) {
+    public ChaosLeftAuto(Drivetrain drivetrain, Feeder feeder, Flywheel flywheel, Hood hood) {
         Optional<Trajectory<SwerveSample>> chaosLeftBackup = Choreo.loadTrajectory(
             "ChaosLeftBackup"
         );
         Optional<Trajectory<SwerveSample>> chaosLeftChaos = Choreo.loadTrajectory(
             "ChaosLeftChaos"
         );
-        ManualShoot shooter = new ManualShoot(
-            new Flywheel(new FlywheelIO() {}),
-            new Feeder(new FeederIO() {}, new FeederIO() {}),
-            100
-        );
 
         addCommands(
             new FollowPath(chaosLeftBackup.get(), drivetrain, true),
-            new InstantCommand(() -> shooter.initialize()),
-            new WaitCommand(2.0),
-            new InstantCommand(() -> shooter.end(false)),
+            new InstantCommand(hood::stow, hood),
+            new ManualShoot(flywheel, feeder, 100)
+                .withTimeout(2.0),
+            new ShootCommands.Shoot(feeder)
+                .withTimeout(4.0),
             new FollowPath(chaosLeftChaos.get(), drivetrain, false)
         );
 
-        addRequirements(drivetrain);
-        // TODO: add shooter and other requirements here
+        addRequirements(drivetrain, feeder, flywheel, hood);
     }
 }
