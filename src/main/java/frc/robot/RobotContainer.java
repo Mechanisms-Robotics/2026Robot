4782d7b4d152327365a9aaf3ec8651427e6e8d5d
Micah Maphet <micahmaphet@gmail.com>
2026-03-24 16:46:25 -0400

    fix buttons

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -183,13 +183,13 @@ public class RobotContainer {
             this.drivetrain.poseEstimator,
             this.shotCalculator,
             // shoot button
-            this.controller.button(1), // right trigger
+            this.controller.R2(), // right trigger
             // intake button
             this.controller.L2(), // left trigger
             // manual mode toggle
-            this.controller.button(3), // right bumper
+            this.controller.R1(), // right bumper
             // stow intake button
-            this.controller.L1() // right bumper
+            this.controller.L1() // left bumper
         );
 
         configureBindings();
