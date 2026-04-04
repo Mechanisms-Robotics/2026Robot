2296ac2d124e89f77bd2f0a05bc04ffab2094ad4
lukewhite32 <lukebenwhite32@gmail.com>
2026-04-04 11:59:02 -0400

    camera offsets and intake tested

--- src/main/java/frc/robot/CONSTANTS.java
+++ src/main/java/frc/robot/CONSTANTS.java
@@ -109,9 +109,9 @@ public class CONSTANTS {
     public static final double WIDTH_METERS = 0.695;
 
     public static final Transform3d CAMERA1_TRANSFORM3D = new Transform3d(
-        LENGTH_METERS / 2.0 - Units.inchesToMeters(12.5), // forward distances from the center of the robot
+        -LENGTH_METERS / 2.0 + Units.inchesToMeters(10.0), // forward distances from the center of the robot
         WIDTH_METERS / 2.0, // leftward distance from the center of the robot
-        Units.inchesToMeters(19.0 + 3.0 / 8.0), 
+        Units.inchesToMeters(18.5), //tuned
         new Rotation3d(
             0, 
             0, 
@@ -121,9 +121,9 @@ public class CONSTANTS {
 
     public static final String CAMERA2_NAME = "PhotonCameraRight";
     public static final Transform3d CAMERA2_TRANSFORM3D = new Transform3d(
-        LENGTH_METERS / 2.0 - Units.inchesToMeters(15.0),
+        -LENGTH_METERS / 2.0 + Units.inchesToMeters(9.5),
         -WIDTH_METERS / 2.0, 
-        Units.inchesToMeters(19.0 + 3.0 / 8.0), 
+        Units.inchesToMeters(18.5), 
         new Rotation3d(
             0,
             0,
@@ -173,8 +173,8 @@ public class CONSTANTS {
 
     // MARK: Intake
     public static class IntakeConstants {
-        public static final double ROLLERS_DUTY_CYCLE = -0.95;
-        public static final double ROLLERS_IDLE_DUTY_CYCLE = -0.3;
+        public static final double ROLLERS_DUTY_CYCLE = -0.5;
+        public static final double ROLLERS_IDLE_DUTY_CYCLE = -0.1;
 
         public static final int ARM_CAN_ID_LEFT = 12;
         public static final int ARM_CAN_ID_RIGHT = 13;
